e2a04a32782c3b51a8f9c3e2755ea92f7987a85f
npawar3 <npawar3@binghamton.edu>
2018-10-26 17:24:16 -0400

    change1

--- controller.py
+++ controller.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+sys.path.append('/home/vchaska1/protobuf/protobuf-3.5.1/python')
+import addressbook_pb2
 import sys
 import socket
 fname='testfile.txt'
@@ -7,13 +9,17 @@ fname='testfile.txt'
 #branch= file.read() 
 
 with open(fname) as f:
-    branch= f.readlines()  
-for each in branch:  
-    if each:
+    for each in f:  
+    
       print each
-      ip= each.split()[1]
+      ip= each.split()[1]      
       port=each.split()[2]
+      print port
+      inport=int(port.strip('\0'))
       s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-      
-      s.connect((ip,int(port)))
+      #BR=BranchMessage()
+      #branch=BR.init_branch.add()
+      #branch.balance=100 
+      s.connect((ip,int(inport)))
       s.send("message send from controler")
+      print s.recv(1024)
