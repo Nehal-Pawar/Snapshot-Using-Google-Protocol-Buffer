94f7de938bb1c36d3cccec9583f17b86a66628ac
Vidisha Kudalkar <vkudalk1@cs.binghamton.edu>
2018-10-26 18:10:43 -0400

    bank_pb2 code added to branch

--- branch.py
+++ branch.py
@@ -2,6 +2,10 @@ import socket
 import os
 import sys
 
+sys.path.append('/home/vchaska1/protobuf/protobuf-3.5.1/python')
+
+
+import bank_pb2
 # No command line arguments needed
 if len(sys.argv) != 4:
 	print("Usage:", sys.argv[0], "Branch name", "Port number", "Maximum Interval in ms")
@@ -14,7 +18,7 @@ serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.bind(('',int(sys.argv[2])))
 
 # Print host name and port number of server
-host_name = socket.gethostname()
+host_name = socket.gethostbyname(socket.gethostname())
 server_port = serverSocket.getsockname()[1]
 print host_name + ' listening on port ' + str(server_port)
 
@@ -27,9 +31,12 @@ while 1:
 
 	# Receive the message
 	msg = clientSocket.recv(1024)
-	#bankdetails.ParseFromString(msg)
+	bankdetails = bank_pb2.BranchMessage
 
-	print msg
+	bankdetails.ParseFromString(msg)
+
+
+	print bankdetails
 
 	response = 'Message recieved to ' + sys.argv[1]
 	# Send the response to client
