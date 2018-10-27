d3f1314b4b0e356f275f2446387bc8b13e7ab5ab
npawar3 <npawar3@binghamton.edu>
2018-10-26 21:18:24 -0400

    working controler

--- controller.py
+++ controller.py
@@ -7,18 +7,42 @@ import socket
 fname='testfile.txt'
 #file = open(fname, 'r') 
 #branch= file.read() 
+count=0
+BR=bank_pb2.BranchMessage()
+
 
 with open(fname) as f:
     for each in f:  
-    
+      
       print each
       ip= each.split()[1]      
       port=each.split()[2]      
+      inport=int(port.strip('\0'))      
+      bank=BR.init_branch
+      
+      branch1=bank.all_branches.add()
+      #BR=bank_pb2.BranchMessage.add()
+      branch1.name=each.split()[0]
+      branch1.ip=ip
+      branch1.port=inport
+      count=count+1
+
+f.close()
+
+total_balance = int(sys.argv[1])
+bank.balance = total_balance / count
+
+with open(fname) as f:
+    for each in f:  
+      ip= each.split()[1]      
+      port=each.split()[2]
       inport=int(port.strip('\0'))
-      s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-      BR=bank_pb2.BranchMessage()
-      branch=BR.init_branch
-      branch.balance=100
+      s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+      #print ip,inport
       s.connect((ip,int(inport)))
       s.send(BR.SerializeToString())
       print s.recv(1024)
+      
+      s.close()
+      
+f.close()
